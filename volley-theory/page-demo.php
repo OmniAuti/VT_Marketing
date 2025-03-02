b7939a6ed7b0c0967dcf7ff10c13cd016d289266
Austin Olijar <Austin.Olijar@gmail.com>
2025-03-02 11:22:54 -0600

    Add files via upload

--- volley-theory/page-demo.php
+++ volley-theory/page-demo.php
@@ -16,7 +16,161 @@
 get_header();
 ?>
 	<main>
-
+		<section id="demo-page--container" class="section--default">
+			<div class="demo-page-text--container">
+				<h1 class="hero--text">Matchups make the difference.</h1>
+				<p class="section--text">When mental toughness isn’t enough, the team who strategizes best, wins. Analyze all the unique matchups possible between your lineup and your opponent. Coach with a clearer view using VolleyTheory.</p>
+				<img src="http://localhost/VT/wp-content/uploads/images/demo-computer.png" alt="Computer Demo">
+			</div>
+			<div class="demo-page-form--container">
+				<h2 class="section--headline">REQUEST DEMO</h2>
+				<form id="home-demo--form" action="">
+						<div class="form--input-container">
+							<label for="first_name"">First Name</label>
+							<input class="input" type="text" name="first_name" id="first_name">
+						</div>
+						<div class="form--input-container">
+							<label for="last_name">Last Name</label>
+							<input class="input" type="text" name="last_name" id="last_name">
+						</div>
+						<div class="form--input-container">
+							<label for="phone">Phone</label>
+							<input class="input" type="tel" name="phone" id="phone">
+						</div>
+						<div class="form--input-container">
+							<label for="email">Email</label>
+							<input class="input" type="email" name="email" id="email">
+						</div>
+						<div class="form--input-container">
+							<label for="organization_name">Organization Name</label>
+							<input class="input" type="text" name="organization_name" id="organization_name">
+						</div>
+						<div class="form--input-container arrow--select">
+							<label class="arrow--select" for="staff_size">Total Staff Size</label>
+							<select class="input" name="staff_size" id="staff_size">
+								<option selected disabled value=""></option>
+								<option value="0">0</option>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
+								<option value="8">8</option>
+								<option value="9">9</option>
+								<option value="10">10</option>
+								<option value="11">11</option>
+								<option value="12">12</option>
+								<option value="13">13</option>
+								<option value="14">14</option>
+								<option value="15">15</option>
+								<option value="16">16</option>
+								<option value="17">17</option>
+								<option value="18">18</option>
+								<option value="19">19</option>
+								<option value="20">20</option>
+								<option value="21">21</option>
+								<option value="22">22</option>
+								<option value="23">23</option>
+								<option value="24">24</option>
+								<option value="25">25</option>
+								<option value="26">26</option>
+								<option value="27">27</option>
+								<option value="28">28</option>
+								<option value="29">29</option>
+								<option value="30">30</option>
+								<option value="31">31</option>
+								<option value="32">32</option>
+								<option value="33">33</option>
+								<option value="34">34</option>
+								<option value="35">35</option>
+								<option value="36">36</option>
+								<option value="37">37</option>
+								<option value="38">38</option>
+								<option value="39">39</option>
+								<option value="40">40</option>
+								<option value="41">41</option>
+								<option value="42">42</option>
+								<option value="43">43</option>
+								<option value="44">44</option>
+								<option value="45">45</option>
+								<option value="46">46</option>
+								<option value="47">47</option>
+								<option value="48">48</option>
+								<option value="49">49</option>
+								<option value="50">50</option>
+								<option value="51">51</option>
+								<option value="52">52</option>
+								<option value="53">53</option>
+								<option value="54">54</option>
+								<option value="55">55</option>
+								<option value="56">56</option>
+								<option value="57">57</option>
+								<option value="58">58</option>
+								<option value="59">59</option>
+								<option value="60">60</option>
+								<option value="61">61</option>
+								<option value="62">62</option>
+								<option value="63">63</option>
+								<option value="64">64</option>
+								<option value="65">65</option>
+								<option value="66">66</option>
+								<option value="67">67</option>
+								<option value="68">68</option>
+								<option value="69">69</option>
+								<option value="70">70</option>
+								<option value="71">71</option>
+								<option value="72">72</option>
+								<option value="73">73</option>
+								<option value="74">74</option>
+								<option value="75">75</option>
+								<option value="76">76</option>
+								<option value="77">77</option>
+								<option value="78">78</option>
+								<option value="79">79</option>
+								<option value="80">80</option>
+								<option value="81">81</option>
+								<option value="82">82</option>
+								<option value="83">83</option>
+								<option value="84">84</option>
+								<option value="85">85</option>
+								<option value="86">86</option>
+								<option value="87">87</option>
+								<option value="88">88</option>
+								<option value="89">89</option>
+								<option value="90">90</option>
+								<option value="91">91</option>
+								<option value="92">92</option>
+								<option value="93">93</option>
+								<option value="94">94</option>
+								<option value="95">95</option>
+								<option value="96">96</option>
+								<option value="97">97</option>
+								<option value="98">98</option>
+								<option value="99">99</option>
+								<option value="100">100</option>
+								<option value="100+">100+</option>
+							</select>
+						</div>
+						<div class="form--input-container arrow--select">
+							<label for="organization_level">Organizational Level</label>
+							<select class="input" name="organization_level" id="organization_level">
+								<option selected disabled value=""></option>
+								<option value="recreational">Recreational</option>
+								<option value="middle_school">Middle School</option>
+								<option value="high_school">High School</option>
+								<option value="college">College</option>
+								<option value="professional">Professional</option>
+							</select>
+						</div>
+						<div class="home-form--submit">
+							<span></span>
+							<input type="submit" value="Submit">
+						</div>
+				</form>
+			</div>
+		</section>
 	</main>
 	<script>
 		const labels = document.querySelectorAll("form label");
--- volley-theory/page-pricing.php
+++ volley-theory/page-pricing.php
@@ -16,7 +16,36 @@
 get_header();
 ?>
 	<main>
-
+		<section id="pricing-page--container" class="section--default">
+			<h1 class="hero--text">Outsmart. Outlast. Outplay.</h1>
+			<article class="quote-box--container demo-quote-page--container">
+			
+				<div class="demo-quote-price">
+					<p>VOLLEY THEORY</p>
+					<p>$1,500</p>
+					<a class="link--btn" href="/demo">Purchase Now</a>
+				</div>
+				
+				<div class="demo-quote-details">
+					<div>
+						<p>Unlimited match analysis,  in-season and post-season</p>
+						<p>Unlimited line-up prediction</p>
+					</div>
+					<div>
+						<p>One-Year Subscription August 1st - July 31 price increases Aug 8</p>
+						<p>Easy to use + always with you–at your desk or on the court</p>
+					</div>
+				</div>
+			</article>
+		<div class="pricing-page-text--container">
+			<img src="http://localhost/VT/wp-content/uploads/images/VolleyTheory_ProductMockup_Login.svg" alt="Login Example">
+			<div >
+				<h3 class="section--subheadline">Winning starts with the right matchup</h3>
+				<p class="section--text">Volley Theory gives you the complete view of all the unique matchups possible between your lineup and any other team you play. No other platform offers this insight.</p>
+				<a class="link--btn" href="/demo">Schedule demo</a>
+			</div>
+		</div>
+		</section>
 	</main>
 	<?php 
 	get_footer(); 
